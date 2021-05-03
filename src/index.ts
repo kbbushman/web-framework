901e9c9ffbdb0e9cd30a4d2a1db5a8ea49520c77
kbbushman <k.bushman@yahoo.com>
2021-05-02 23:02:05 -0700

    Adds User model with get/set property methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello Dave...');
+import { User } from './models/User';
+
+const user = new User({ name: 'John Doe', age: 22 });
+user.set({ name: 'Kevin Smith' });
+console.log(user.get('name'));
+console.log(user.get('age'));
