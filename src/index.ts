559843ae4cbe3ce7c884d1c7e6e28ba61f63382f
kbbushman <k.bushman@yahoo.com>
2021-05-02 23:19:03 -0700

    Adds User event system

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'John Doe', age: 22 });
-user.set({ name: 'Kevin Smith' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('poop', () => {
+  console.log('Poop!');
+});
+
+user.trigger('asdfasdf');
