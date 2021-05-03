0bd45844d6d814906c68ba918e8361955cc1ba22
kbbushman <k.bushman@yahoo.com>
2021-05-03 00:12:17 -0700

    Adds User fetch method

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'John Doe', age: 22 });
-
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('poop', () => {
-  console.log('Poop!');
-});
-
-user.trigger('asdfasdf');
+const user = new User({ id: 1 });
+user.fetch();
