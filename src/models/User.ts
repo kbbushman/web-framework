584239157bbc055f1c04dbea56276ba447ecd0b5
kbbushman <k.bushman@yahoo.com>
2021-05-04 22:55:56 -0700

    Adds save method

--- src/models/User.ts
+++ src/models/User.ts
@@ -44,4 +44,16 @@ export class User {
         this.set(response.data);
       });
   }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      // put
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      // post
+      axios.post('http://localhost:3000/users', this.data);
+    }
+  }
 }
