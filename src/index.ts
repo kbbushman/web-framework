afa62c9c771961c97eee3b54cfbb7b690d97deda
kbbushman <k.bushman@yahoo.com>
2021-05-06 23:07:08 -0700

    Adds Eventing as events on User

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'New Person', age: 33 });
+const user = new User({ name: 'Person 5', age: 0 });
 // user.set({ name: 'New Name', age: 44 });
-user.save();
+user.events.on('change', () => {
+  console.log('change');
+});
+
+user.events.trigger('change');
