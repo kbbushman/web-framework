657d400ee2c432e9f4eb9c335f02e36327228120
kbbushman <k.bushman@yahoo.com>
2021-05-07 23:02:25 -0700

    Adds Sync class for api calls

---
+++ src/models/Sync.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      // put
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      // post
+      return axios.post(`${this.rootUrl}`, data);
+    }
+  }
+}
