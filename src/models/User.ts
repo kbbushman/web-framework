bebcc66d617be2c75131d2f7da6b62194d270dd8
kbbushman <k.bushman@yahoo.com>
2021-05-07 23:02:53 -0700

    Refactors fetch/save methods to Sync

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
@@ -18,25 +17,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      // post
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
