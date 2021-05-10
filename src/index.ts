f1ee6505da96cab2c91719b5c30a97a4239159d8
kbbushman <k.bushman@yahoo.com>
2021-05-09 17:32:14 -0700

    Tests User methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Person 5', age: 0 });
-// user.set({ name: 'New Name', age: 44 });
-user.events.on('change', () => {
-  console.log('change');
+const user = new User({ id: 1, name: 'newiest name', age: 44 });
+
+user.on('save', () => {
+  console.log(user);
 });
 
-user.events.trigger('change');
+user.save();
