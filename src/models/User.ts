47e5cac2aab31d0cb23746c034feec1d9446a2c5
kbbushman <k.bushman@yahoo.com>
2021-05-11 23:31:44 -0700

    Adds static buildUserCollection method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Collection } from './Collection';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
 import { Attributes } from './Attributes';
@@ -17,6 +18,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
