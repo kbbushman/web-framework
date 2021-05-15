269f8e56beca70879fd394ddaecc05c3fbfef229
kbbushman <k.bushman@yahoo.com>
2021-05-15 15:47:21 -0700

    Adds input type check in set name method

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -21,9 +21,11 @@ export class UserForm {
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
 
-    const name = input.value;
+    if (input) {
+      const name = input.value;
 
-    this.model.set({ name });
+      this.model.set({ name });
+    }
   };
 
   onSetAgeClick = (): void => {
