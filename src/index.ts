98b548efa94dc99e06ca3b9425b13e0026206d2b
kbbushman <k.bushman@yahoo.com>
2021-05-15 17:33:01 -0700

    Adds user edit view

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
-import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'NAME', age: 23 });
 
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
 } else {
   throw new Error('Root element not found');
 }
