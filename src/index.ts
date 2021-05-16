1d0473fdd7635525479993e2a9734291e1f01eeb
kbbushman <k.bushman@yahoo.com>
2021-05-15 17:53:36 -0700

    Adds UserList view

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
+import { User, UserProps } from './models/User';
+// import { UserEdit } from './views/UserEdit';
 
-const user = User.buildUser({ name: 'NAME', age: 23 });
+// const user = User.buildUser({ name: 'NAME', age: 23 });
 
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
 
-  userEdit.render();
-} else {
-  throw new Error('Root element not found');
-}
+//   userEdit.render();
+// } else {
+//   throw new Error('Root element not found');
+// }
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
